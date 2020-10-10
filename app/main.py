df9a0f6308c46bfc8e2153229dcbee8644c73607
C. Kristensen <cjckris@gmail.com>
2020-10-10 22:21:26 +0200

    updated endpoints

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ import logging
 
 AZURE_STORAGE_CONNECTION_STRING="DefaultEndpointsProtocol=https;AccountName=goolit2;AccountKey=iOHs5Z9m+/S4doMBH5G/JQzbO/6vx+V6UiRw9FQw3gHypjLoZqu0tMvh9WOezkMWEBSyz+ZMaFcvjZyiMHft9Q==;EndpointSuffix=core.windows.net"
 
-
 logging.basicConfig(filename="log.log", level=logging.INFO)
 
 app = FastAPI()
@@ -16,11 +15,34 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/status")
+def read_item():
+    return {"Status": "OK"}
 
-@app.get("/sentence/{sentence}")
+@app.get("/refresh")
+def get_prediction():
+    logging.info('get prediction')
+    connect_str = AZURE_STORAGE_CONNECTION_STRING
+    
+    logging.info('done1')
+    # Create the BlobServiceClient object which will be used to create a container client
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+    logging.info('done2')
+    # List the blobs in the container
+    blob_client = blob_service_client.get_blob_client(container="model", blob="compressed_model.zip")
+    
+    logging.info('done3')
+    stream = blob_client.download_blob()
+    logging.info('done4')
+
+    logging.info('Started get zip') 
+    with open('cm.zip', 'ab') as fp:
+        b = stream.content_as_bytes()
+        fp.write(b)
+    
+    return {"Refresh": "OK"}
+
+@app.get("/predict/{sentence}")
 def get_prediction(sentence: str):
     logging.info('get prediction')
     connect_str = AZURE_STORAGE_CONNECTION_STRING
@@ -46,6 +68,5 @@ def get_prediction(sentence: str):
         myzip.extractall()
         model = tf.keras.models.load_model('.')
     
-
     logging.info('Starting prediction')
     return {'Sentence': str(sentence), 'Sentence score': str(float(model.predict([[sentence]])))}
